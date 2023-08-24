1baffeb7721c42d2de0d47279197908dbba42738
sasfog <aboncser@edu.bme.hu>
2023-08-24 09:41:09 +0200

    Added single cmaera version of image regions widget

---
+++ src/camImageRegionsWidget.cpp
@@ -0,0 +1,141 @@
+#include "camImageRegionsWidget.h"
+
+
+CamImageRegionsWidget::CamImageRegionsWidget(QWidget *parent) : 
+    QFrame(parent),
+    applicationSettings(new QSettings(QSettings::IniFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName(), parent)) {
+    
+    QHBoxLayout *vlayout = new QHBoxLayout;
+
+    setLayout(vlayout);
+    setFrameStyle(2);
+
+    
+    // widgetBorderPen.setAlphaF( 0.3 );
+    // penAutoParamAccent.setStyle(Qt::DashLine);
+    // penAutoParamBack.setStyle(Qt::SolidLine);
+
+    penWidgetBorder = QPen(Qt::black, 1, Qt::DashLine);
+
+    penImageAcqROI1 = QPen(Qt::darkRed, 1, Qt::DashLine);
+    penImageAcqROI2 = QPen(Qt::darkRed, 1, Qt::DashLine);
+
+    penPupilDetectionROI1 = QPen(Qt::blue, 1, Qt::SolidLine);
+    penPupilDetectionROI2 = QPen(Qt::green, 1, Qt::SolidLine);
+    // penPupilDetectionROI1 = QPen(Qt::cyan, 1, Qt::SolidLine);
+    // penPupilDetectionROI2 = QPen(Qt::yellow, 1, Qt::SolidLine);
+
+    penImageAcqROISplitLine = QPen(Qt::black, 1, Qt::DashLine);
+
+    drawingArea = QRect(0, 0, size().width(), size().height());
+}
+
+CamImageRegionsWidget::~CamImageRegionsWidget(void) {
+
+}
+
+void CamImageRegionsWidget::setImageMaxSize(QSize size) {
+    imageMaxSize = size;
+    update();
+}
+void CamImageRegionsWidget::setImageAcqROI1Rect(QRect rect) {
+    imageAcqROI1Rect = rect;
+    update();
+}
+void CamImageRegionsWidget::setImageAcqROI2Rect(QRect rect) {
+    imageAcqROI2Rect = rect;
+    update();
+}
+void CamImageRegionsWidget::setPupilDetectionROI1Rect(QRect rect) {
+    pupilDetectionROI1Rect = rect;
+    update();
+}
+void CamImageRegionsWidget::setPupilDetectionROI2Rect(QRect rect) {
+    pupilDetectionROI2Rect = rect;
+    update();
+}
+void CamImageRegionsWidget::setUsingMultipleROI(bool state) {
+    usingMultipleROI = state;
+    update();
+}
+
+QRect CamImageRegionsWidget::toDrawingArea(QRect rect) {
+    return QRect(
+        (float)rect.x()/imageMaxSize.width() *drawingArea.width() + drawingArea.x(), 
+        (float)rect.y()/imageMaxSize.height() *drawingArea.height() + drawingArea.y(), 
+        (float)rect.width()/imageMaxSize.width() *drawingArea.width(), 
+        (float)rect.height()/imageMaxSize.height() *drawingArea.height()
+        );
+}
+QLine CamImageRegionsWidget::toDrawingArea(QLine line) {
+    return QLine(
+        (float)line.x1()/imageMaxSize.width() *drawingArea.width() + drawingArea.x(), 
+        (float)line.y1()/imageMaxSize.height() *drawingArea.height() + drawingArea.y(), 
+        (float)line.x2()/imageMaxSize.width() *drawingArea.width() + drawingArea.x(), 
+        (float)line.y2()/imageMaxSize.height() *drawingArea.height() + drawingArea.y()
+        );
+}
+
+
+void CamImageRegionsWidget::resizeEvent(QResizeEvent *event) {
+    // calculate aspect ratios for "fit in" view of the sensor size (image max size) in the widget
+    float widgetAR = (float)size().width()/size().height();
+    float sensorAR = (float)imageMaxSize.width()/imageMaxSize.height();
+
+    // -1 is needed because we cannot draw at the rightmost and bottom pixel lines of the widget sometimes
+    if(widgetAR >= sensorAR) {
+        // up and bottom edges of drawingArea touch the border of the widget
+        // shrink the width of drawingArea AND center horizontally
+        qDebug() << "1";
+        drawingArea = QRect(
+            0 + (float)event->size().width()/2 - ((float)event->size().height() * sensorAR /2), 
+            0, 
+            (float)event->size().height() * sensorAR -1, 
+            event->size().height() -1 );
+    } else {
+        // left and right edges of drawingArea touch the border of the widget
+        // shrink the height of drawingArea AND center vertically
+        qDebug() << "2";
+        drawingArea = QRect(
+            0, 
+            0 + (float)event->size().height()/2 - ((float)event->size().width() / sensorAR /2), 
+            event->size().width() -1, 
+            (float)event->size().width() / sensorAR -1 );
+    }
+}
+
+void CamImageRegionsWidget::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+
+    painter.setPen(penWidgetBorder);
+    painter.drawRect(toDrawingArea(QRect(0,0,imageMaxSize.width(),imageMaxSize.height())));
+
+    if(!imageAcqROI1Rect.isEmpty()) {
+        painter.setPen(penImageAcqROI1);
+        painter.drawRect(toDrawingArea(imageAcqROI1Rect));
+    }
+
+    if(!imageAcqROI2Rect.isEmpty()) {
+        painter.setPen(penImageAcqROI2);
+        painter.drawRect(toDrawingArea(imageAcqROI2Rect));
+    }
+
+    if(!pupilDetectionROI1Rect.isEmpty()) {
+        painter.setPen(penPupilDetectionROI1);
+        painter.drawRect(toDrawingArea(pupilDetectionROI1Rect));
+    }
+
+    if(!pupilDetectionROI2Rect.isEmpty()) {
+        painter.setPen(penPupilDetectionROI2);
+        painter.drawRect(toDrawingArea(pupilDetectionROI2Rect));
+    }
+
+    if(usingMultipleROI) {
+        painter.setPen(penImageAcqROISplitLine);
+        painter.drawLine(toDrawingArea(QLine((float)imageMaxSize.width()/2,0,(float)imageMaxSize.width()/2,(float)imageMaxSize.height())));
+    }
+
+}
+
---
+++ src/camImageRegionsWidget.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QPen>
+#include <QLine>
+
+#include <QHBoxLayout>
+#include <QResizeEvent>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QSettings>
+#include <QCoreApplication>
+#include <QString>
+#include <QDebug>
+
+class CamImageRegionsWidget : public QFrame {
+Q_OBJECT
+
+public:
+    CamImageRegionsWidget(QWidget *parent = nullptr);
+    ~CamImageRegionsWidget(void) override;
+
+    void setImageMaxSize(QSize size);
+    void setImageAcqROI1Rect(QRect rect);
+    void setImageAcqROI2Rect(QRect rect);
+    void setPupilDetectionROI1Rect(QRect rect);
+    void setPupilDetectionROI2Rect(QRect rect);
+    void setUsingMultipleROI(bool state);
+
+    void resizeEvent(QResizeEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    
+    QSettings *applicationSettings;
+
+    // TODO option 1: 
+    // use setScale and translate using Qt things, not QRect and QLine separately but any QGraphicsItem
+    // TODO option 2: 
+    // discard most of this code and use a QGraphicsScene that is automatically fitted in a view that fills the widget
+    QRect toDrawingArea(QRect rect);
+    QLine toDrawingArea(QLine line);
+
+    QPen penWidgetBorder;
+    QPen penImageAcqROI1;
+    QPen penImageAcqROI2;
+    QPen penPupilDetectionROI1;
+    QPen penPupilDetectionROI2;
+    QPen penImageAcqROISplitLine;
+
+    bool usingMultipleROI = false;
+
+    QRect drawingArea;
+
+    QSize imageMaxSize;
+
+    QRect imageAcqROI1Rect;
+    QRect imageAcqROI2Rect;
+    QRect pupilDetectionROI1Rect;
+    QRect pupilDetectionROI2Rect;
+
+};
+
